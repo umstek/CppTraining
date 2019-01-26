71c576012af0959caa3f68d2a42ac61debbc7d06
Wickramaranga Abeygunawardhana <umstek@live.com>
2019-01-26 18:40:50 +0530

    Add dynamic cast

--- CppTraining/CppTraining.cpp
+++ CppTraining/CppTraining.cpp
@@ -5,8 +5,66 @@
 
 using namespace std;
 
+class A
+{
+public:
+	A();
+	~A();
+	int GetA() const {
+		return a;
+	}
+	int GetAA() const {
+		return  aa;
+	}
+	virtual void M() const {
+		cout << "M from A" << endl;
+	}
+protected:
+	int a;
+private:
+	int aa;
+};
+
+A::A()
+{
+}
+
+A::~A()
+{
+}
+
+class B : public A
+{
+public:
+	B();
+	~B();
+	virtual void M() const {
+		cout << "M from A" << endl;
+	}
+private:
+
+};
+
+B::B()
+{
+}
+
+B::~B()
+{
+}
+
+void DynamicCastExample() {
+	A *ba = new B();
+	B *bb = dynamic_cast <B*>(ba);
+	ba->M();
+	bb->M();
+}
+
 int main()
 {
-	cout << "Hello CMake." << endl;
+
+	DynamicCastExample();
+
+	system("pause");
 	return 0;
 }
